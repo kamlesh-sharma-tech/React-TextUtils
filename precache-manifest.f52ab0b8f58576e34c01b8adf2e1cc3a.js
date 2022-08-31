9730ceecd5c3b032372e9d8a291808dc02fd391d
Kamlesh Sharma <theshield010160@gmail.com>
2022-08-31 12:43:03 +0530

    Updates

---
+++ precache-manifest.f52ab0b8f58576e34c01b8adf2e1cc3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "95567c9a17e3ce95069c",
+    "url": "/React-TextUitls/static/css/main.59cd46e7.chunk.css"
+  },
+  {
+    "revision": "95567c9a17e3ce95069c",
+    "url": "/React-TextUitls/static/js/main.95567c9a.chunk.js"
+  },
+  {
+    "revision": "6a7bfa4df9242b40edff",
+    "url": "/React-TextUitls/static/js/1.6a7bfa4d.chunk.js"
+  },
+  {
+    "revision": "7a03ad1f33867b0cbdea",
+    "url": "/React-TextUitls/static/js/2.7a03ad1f.chunk.js"
+  },
+  {
+    "revision": "8180b721ec814be8329b",
+    "url": "/React-TextUitls/static/js/runtime~main.8180b721.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/React-TextUitls/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "1e577a8c417297e2e4d3d9d37dba3927",
+    "url": "/React-TextUitls/index.html"
+  }
+];
